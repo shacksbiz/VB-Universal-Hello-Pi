61d0e2ff54255e46cab3270cae6544051a0e7f95
shacksbiz <36748216+shacksbiz@users.noreply.github.com>
2018-02-24 11:43:20 -0600

    Add files via upload

---
+++ MainPage.xaml.vb
@@ -0,0 +1,15 @@
+﻿' The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+''' <summary>
+''' An empty page that can be used on its own or navigated to within a Frame.
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Page
+    Public Sub New()
+        InitializeComponent()
+        'add startup stuff here
+    End Sub
+
+    Private Sub TextBox_TextChanged(sender As Object, e As TextChangedEventArgs)
+
+    End Sub
+End Class
